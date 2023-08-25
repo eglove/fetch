06000d15e79cc8640a402e4b0d37dc3389cf059b
Ethan Glover <hello@ethang.email>
2023-08-24 21:01:59 -0500

    "Add URL builder tests
    New tests have been added to verify the correct working of the URL builder utility. These tests cover both the proper formation of URLs and the handling of search parameters. Ensuring these features work as expected will facilitate the robust and systematic construction of URLs in our codebase."

---
+++ tests/url.test.ts
@@ -0,0 +1,119 @@
+import { describe, expect, test } from 'vitest';
+
+import { urlBuilder } from '../src/url-builder';
+
+describe('url', () => {
+  const baseUrl = 'https://developer.mozilla.org';
+
+  test('creates url correctly', () => {
+    const A = urlBuilder('/', { urlBase: baseUrl });
+    expect(A.toString()).toBe('https://developer.mozilla.org/');
+
+    const B = urlBuilder(baseUrl);
+    expect(B.toString()).toBe('https://developer.mozilla.org/');
+
+    expect(urlBuilder('en-US/docs', { urlBase: B.toString() }).toString()).toBe(
+      'https://developer.mozilla.org/en-US/docs',
+    );
+
+    const D = urlBuilder('/en-US/docs', { urlBase: B.toString() });
+    expect(D.toString()).toBe('https://developer.mozilla.org/en-US/docs');
+
+    expect(
+      urlBuilder('/en-US/docs', { urlBase: D.toString() }).toString(),
+    ).toBe('https://developer.mozilla.org/en-US/docs');
+
+    expect(
+      urlBuilder('/en-US/docs', { urlBase: A.toString() }).toString(),
+    ).toBe('https://developer.mozilla.org/en-US/docs');
+
+    expect(
+      urlBuilder('/en-US/docs', {
+        urlBase: 'https://developer.mozilla.org/fr-FR/toto',
+      }).toString(),
+    ).toBe('https://developer.mozilla.org/en-US/docs');
+  });
+});
+
+describe('searchParameters', () => {
+  const urlString = 'https://example.com';
+
+  const parameters = [
+    {
+      q: 'URLUtils.searchParams',
+      topic: 'api',
+    },
+    'q=URLUtils.searchParams&topic=api',
+  ];
+
+  test.each(parameters)('works with params correctly', parameters => {
+    const builder = urlBuilder(urlString, {
+      searchParams: parameters,
+    });
+
+    expect(builder.searchParameters.has('topic')).toBe(true);
+    expect(builder.searchParameters.has('topic', 'fish')).toBe(false);
+    expect(builder.searchParameters.get('topic') === 'api').toBe(true);
+    expect(builder.searchParameters.getAll('topic')).toStrictEqual(['api']);
+    expect(builder.searchParameters.get('foo')).toBeNull();
+
+    builder.searchParameters.append('topic', 'webdev');
+    expect(builder.searchParameters.toString()).toBe(
+      'q=URLUtils.searchParams&topic=api&topic=webdev',
+    );
+
+    builder.searchParameters.set('topic', 'More webdev');
+    expect(builder.searchParameters.toString()).toBe(
+      'q=URLUtils.searchParams&topic=More+webdev',
+    );
+
+    builder.searchParameters.delete('topic');
+    expect(builder.searchParameters.toString()).toBe('q=URLUtils.searchParams');
+  });
+
+  test('handles duplicate parameters', () => {
+    const builder = urlBuilder(urlString, { searchParams: 'foo=bar&foo=baz' });
+
+    expect(builder.searchParameters.toString()).toBe('foo=bar&foo=baz');
+    expect(builder.searchParameters.has('foo')).toBe(true);
+    expect(builder.searchParameters.get('foo')).toBe('bar');
+    expect(builder.searchParameters.getAll('foo')).toStrictEqual([
+      'bar',
+      'baz',
+    ]);
+  });
+
+  test('treats full url as query', () => {
+    const parametersString = 'https://notexample.com/search?query=%40';
+    const { searchParameters } = urlBuilder(urlString, {
+      searchParams: parametersString,
+    });
+
+    expect(searchParameters.has('query')).toBe(false);
+    expect(searchParameters.has('https://notexample.com/search?query')).toBe(
+      true,
+    );
+    expect(searchParameters.get('query')).toBeNull();
+    expect(searchParameters.get('https://notexample.com/search?query')).toBe(
+      '@',
+    );
+  });
+
+  test('strips ?', () => {
+    const parametersString = '?query=value';
+    const { searchParameters } = urlBuilder(urlString, {
+      searchParams: parametersString,
+    });
+
+    expect(searchParameters.has('query')).toBe(true);
+  });
+
+  test.each(['foo=&bar=baz', 'foo&bar=baz'])('equal is implied', string => {
+    const { searchParameters } = urlBuilder(urlString, {
+      searchParams: string,
+    });
+
+    expect(searchParameters.get('foo')).toBe('');
+    expect(searchParameters.toString()).toBe('foo=&bar=baz');
+  });
+});
